eb6b7a73888f30e1497ba9e22d0059e78f063806
Marco Arredondo <marcoarredondoz28@gmail.com>
2020-07-17 19:18:54 -0500

    *Adding main screen with menu options. *Adding style and floating elements in mains screen

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,9 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name') }}</title>
-        
+
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap" rel="stylesheet">
+        <!--<link href="https://fonts.googleapis.com/css2?family=Meddon&display=swap" rel="stylesheet">-->
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script>window.Laravel = {csrfToken: '{{ csrf_token() }}'}</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,42 @@
-@extends('layout.main')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
 
-@section('content')
-    <div id="root">
-        
+<body>
+    <div class="shadow"></div>
+
+    <div class="container res-margin-top" >
+        <div class="row">
+
+            <div class="col-lg-6 col-sm-12" align="center" style="margin-bottom: 1rem;">
+                <button class="floatElement btn btn-primary">¿Qué es el Tarot?</button>
+            </div>
+
+            <div class="col-lg-6 col-sm-12" align="center" style="margin-bottom: 1rem;">
+                <button class="floatElement btn btn-primary">Hacer una tirada</button>
+            </div>
+
+        </div>
+
+        <div class="row">            
+            <div class="col-lg-6" align="center" style="margin-bottom: 1rem;">
+                <button class="floatElement btn btn-primary">Los arcanos mayores</button>
+            </div>
+
+
+            <div class="col-lg-6" align="center" style="margin-bottom: 1rem;">
+                <button class="floatElement btn btn-primary">Los arcanos menores</button>
+            </div>
+        </div>
     </div>
-@endsection
+
+  
+</body>
+
+</html>
